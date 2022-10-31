ec04a49a988cb6249c64964a82b9c710e3a851de
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-01 01:39:25 +0530

    .

--- js/common.js
+++ js/common.js
@@ -5,8 +5,8 @@
 // Verify If jquery is loaded
 window.jQuery || document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>');
 
-// Syntax highlighter
-hljs.highlightAll();
+// Syntax highlighter - Enable is using highlight js.
+// hljs.highlightAll();
 
 // Footer Copyright
 function copyright() {
