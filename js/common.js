b782f6ed488b31633f6f0db99ba429fddac655b1
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-08 16:07:46 +0530

    .

--- js/common.js
+++ js/common.js
@@ -160,7 +160,7 @@ function header_author(include_course, is_lab, author_init, include_bio, include
         var button = "";
     }
 
-    course = "<h1>" + course + " (" + course_detail + ")" + header_nav() + "</h2>";
+    course = "<h1>" + course + " (" + course_detail + ") <span>" + header_nav() + "</span></h2>";
 
     document.write(course + "<p>Summarized by " + authorname + author_link + " under the guidance of " + profname + prof_link + "</p>" + prof_bio + author_bio + button);
 }
