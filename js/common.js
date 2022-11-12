eb632d6080924fd2c073036a7ae26f7dd6349205
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-12 21:58:58 +0530

    .

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@
 window.jQuery || document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>');
 
 // Syntax highlighter - Enable is using highlight js.
-window.onload = hljs.highlightAll();
+hljs.highlightAll();
 
 
 /******** Header ***********/
