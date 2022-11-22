cb0d26ed62c68d02b7bac39e7a20bc85c984ddfb
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-23 04:24:48 +0530

    .

--- csu1128/var.js
+++ csu1128/var.js
@@ -58,7 +58,7 @@ function get_menu_list() {
     }
 
     function get_classes() {
-        var cases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+        var cases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 30, 31];
         for (let i = 0; i < cases.length; i++) {
 
             var codetype = "C Language";
