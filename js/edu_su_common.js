b510890007aab575d71688feacea19516c003d1d
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-25 04:25:09 +0530

    .

--- js/edu_su_common.js
+++ js/edu_su_common.js
@@ -329,12 +329,48 @@ function copyright(rights) {
         navigator.serviceWorker.register('/edu/su/sw.js')
     }
 
+    function gen_blockquote() {
+        var data;
+        let front = true;
+        const authors = document.querySelectorAll(".blockquote-footer");
+        const texts = document.querySelectorAll(".blockquote");
+        const authorFront = authors[0];
+        const textFront = texts[0];
+
+        // An arrow function used to get a quote randomly
+        const displayQuote = () => {
+            let index = Math.floor(Math.random() * data.length); // Generates a random number between 0 and the length of the dataset
+            let quote = data[index].text;
+            let author = data[index].author;
+            if (!author) {
+                author = "Anonymous"
+            }
+            textFront.innerHTML = quote;
+            authorFront.innerHTML = author;
+        }
+        // Fetching the quotes from the type.fit API using promises
+        fetch("https://type.fit/api/quotes")
+            .then(function (response) {
+                return response.json();
+            }) // Getting the raw JSON data
+            .then(function (data) {
+
+                // Storing the quotes internally upon
+                // successful completion of request
+                this.data = data;
+
+                // Displaying the quote When the Webpage loads
+                displayQuote()
+            });
+    }
+
     // UselessFunctions -- Notification cookie
     dcevar(notify_cookie);
 
     window.onload = function () {
         // Syntax highlighter - Enable is using highlight js.
         hljs.highlightAll();
+        gen_blockquote();
 
         // Notification - Privacy - I accept
 
