259967bfd423266da247c723b2f80134fc5a5a01
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-24 10:03:18 +0530

    .

--- csu953/var.js
+++ csu953/var.js
@@ -47,42 +47,6 @@ function get_menu_list() {
                     readtime = "2";
                     body_blockcards(link, date, title, desc, codetype, readtime, 1);
                     break;
-                case 3:
-                    link = "c3";
-                    date = "Thu September 27, 2022";
-                    title = "Lab 3";
-                    desc = "Use of tags tags sub, sup, text-align, address, and abbr.";
-                    codetype = "HTML";
-                    readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
-                    break;
-                case 4:
-                    link = "c4";
-                    date = "Thu September 27, 2022";
-                    title = "Lab 4";
-                    desc = "Class Test";
-                    codetype = "HTML";
-                    readtime = "2";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
-                    break;
-                case 5:
-                    link = "c5";
-                    date = "Thu September 27, 2022";
-                    title = "Lab 5";
-                    desc = "Use of table, tr, th tags in creation of Tables";
-                    codetype = "HTML";
-                    readtime = "4";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
-                    break;
-                case 6:
-                    link = "c6";
-                    date = "Thu November 17, 2022";
-                    title = "Lab 6";
-                    desc = "VIVA based on questions directed towards odd and even students.";
-                    codetype = "HTML";
-                    readtime = "5";
-                    body_blockcards(link, date, title, desc, codetype, readtime, 1);
-                    break;
                 default:
                     link = "#";
                     date = "Not Applicable";
