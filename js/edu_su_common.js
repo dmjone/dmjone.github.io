518fe7f13c3bbb4734b5f2819a467ab187aba2cf
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-27 02:07:19 +0530

    .

--- js/edu_su_common.js
+++ js/edu_su_common.js
@@ -63,7 +63,7 @@ function header_navbar() {
             var linkactive = ' active" aria-current="page"';
         }
         linkname = links[i].toUpperCase();
-        li_link += '<li class="nav-item"><a class="nav-link' + linkactive + ' href="/edu/su/' + links[i] + '/">' + linkname + "</a></li>";
+        li_link += '<li class="nav-item"><a class="nav-link' + linkactive + ' href="/edu/su/courses/' + links[i] + '/">' + linkname + "</a></li>";
     }
 
     nav = common_nav_start + li_link + common_nav_end;
