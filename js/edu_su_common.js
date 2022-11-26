50a7031a5053ca63b9f033fbf85ee000932640e3
Divya Mohan <divyamohan1993@users.noreply.github.com>
2022-11-26 23:06:05 +0530

    .

--- js/edu_su_common.js
+++ js/edu_su_common.js
@@ -370,8 +370,8 @@ function gen_blockquote() {
             .then(data => { return data; });
         return data;
     }
-    var data = errt();
-    console.log(data);
+    var data2 = errt();
+    console.log(data2.json());
 
     //    let index = Math.floor(Math.random() * data.length); // Generates a random number between 0 and the length of the dataset
     //  let quote = data[index].text;
